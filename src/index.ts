b8d01dd33c3878fc1e25e3f23698f74d14849d2e
Thomas Wang <hi@xgwang.me>
2022-07-10 18:06:45 -0700

    works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-import { add } from "./add";
+import * as dotenv from "dotenv";
+import { myQApi } from "@hjdhjd/myq";
 
-function main() {
-  console.log(`1 + 2 = ${add(1, 2)}`);
+dotenv.config();
+
+const myQPassword = process.env["MYQ_PASSWORD"];
+const myQUsername = process.env["MYQ_USERNAME"];
+const myQSerialNumber = process.env["MYQ_SERIAL_NUMBER"];
+
+function fail(errorMessage: string) {
+  throw new Error(errorMessage);
+}
+
+async function main() {
+  if (!myQUsername || !myQPassword || !myQSerialNumber) {
+    return fail("credentials missing!");
+  }
+
+  const api = new myQApi(myQUsername, myQPassword);
+  const apiCallSuccessful = await api.refreshDevices();
+  if (!apiCallSuccessful) {
+    return fail("MyQ API call failed!");
+  }
+
+  const device = api.getDevice(myQSerialNumber);
+  if (!device) {
+    return fail("Cannot find device!");
+  }
+
+  const state = device.state.door_state;
+  console.log(`Garage door state: ${state}`);
 }
 
 main();
--- test/add.test.ts
+++ test/add.test.ts
@@ -1,7 +1,5 @@
-import { add } from "../src/add";
-
 describe("add", () => {
   it("should add two numbers", () => {
-    expect(add(1, 2)).toEqual(3);
+    expect(1 + 2).toEqual(3);
   });
 });
